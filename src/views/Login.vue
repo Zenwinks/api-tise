48805c92a3c12589dfc7b5f93d3236ad1c46a132
Zenwinks <salle.lucas@hotmail.fr>
2020-01-08 15:10:19 +0100

    update new login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,20 @@
 <template>
   <div class="login-wrapper">
-    <div class="title">
-      <h1>Connexion</h1>
-    </div>
-    <div class="login">
-      <div class="username">
-        <div class="label">Login :</div>
-        <div class="input">
-          <input id="username" v-model="login" type="text">
-        </div>
+    <div class="login-inner-wrapper">
+      <div class="title">
+        <h1>Connexion</h1>
       </div>
-      <div class="password">
-        <div class="label">Mot de passe :</div>
-        <div class="input">
-          <input id="password" v-model="pwd" type="password">
+      <div class="login">
+        <div class="form-group">
+          <label for="login">Login</label>
+          <input type="text" class="form-control" id="login">
         </div>
-      </div>
-      <div class="error" v-if="error">Le nom d'utilisateur ou le mot de passe est incorrect</div>
-      <div class="button">
-        <div>Se connecter</div>
+        <div class="form-group">
+          <label for="pwd">Password</label>
+          <input type="password" class="form-control" id="pwd">
+        </div>
+        <div class="error" v-if="error">Le nom d'utilisateur ou le mot de passe est incorrect</div>
+        <button type="button" class="btn btn-success btn-lg">Se connecter</button>
       </div>
     </div>
   </div>
@@ -46,82 +42,53 @@
     align-items: center;
     justify-content: center;
 
-    .login {
+    .login-inner-wrapper {
       width: 25%;
-      height: 30%;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      border: 1px solid blue;
-      border-radius: 0 0 10px 10px;
-      font-size: 18px;
+      height: 50%;
+      box-shadow: 0 0 11px black;
 
-      .username, .password {
-        margin-bottom: 20px;
-        display: grid;
-        grid-template-columns: 40% 60%;
+      .login {
         width: 100%;
-        text-align: center;
+        height: 80%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
 
-        .label {
-          grid-column: 1;
-          display: flex;
-          align-items: center;
-          justify-content: flex-end;
-          margin-right: 10px;
+        .error {
+          font-size: 12px;
+          margin-bottom: 20px;
+          color: red;
+          font-weight: bold;
         }
 
-        .input {
-          height: 30px;
-          width: 95%;
-          grid-column: 2;
+        .button {
+          background-color: limegreen;
+          color: white;
+          width: 70%;
+          height: 50px;
+          cursor: pointer;
+          font-weight: bold;
           display: flex;
           align-items: center;
           justify-content: center;
-
-          input {
-            height: 100%;
-            width: 100%;
-          }
+          border-radius: 10px;
         }
       }
 
-      .username {
-        margin-bottom: 15px;
-      }
-
-      .error {
-        font-size: 12px;
-        margin-bottom: 20px;
-        color: red;
-        font-weight: bold;
-      }
-
-      .button {
-        background-color: mediumspringgreen;
-        color: white;
-        width: 70%;
-        height: 50px;
-        cursor: pointer;
-        font-weight: bold;
+      .title {
+        width: 100%;
+        height: 20%;
         display: flex;
         align-items: center;
         justify-content: center;
-        border-radius: 10px;
-      }
-    }
+        background-color: cornflowerblue;
+        color: white;
 
-    .title {
-      width: 25%;
-      text-align: center;
-      background-color: blue;
-      color: white;
-      border: 1px solid blue;
-      border-radius: 10px 10px 0 0;
-
-      h1 {
-        font-size: 22px;
+        h1 {
+          font-size: 22px;
+          margin: 0;
+        }
       }
     }
   }
